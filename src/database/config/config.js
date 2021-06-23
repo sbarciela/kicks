54b14550ec308a2c3c418e7b39af0f272c4a6a86
sbarciela <barcielasebastian@gmail.com>
2021-06-23 19:27:14 -0300

    configurando variables e instalando dotenv

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -6,17 +6,19 @@ module.exports = {
     "password": process.env.DB_PASS,
     "database": process.env.DB_NAME,
     "host": process.env.DB_HOST,
-    "port":process.env.DB_PORT,
     "dialect": "mysql",
     "operatorsAliases": 0
+  
+    
   },
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
     "dialect": "mysql",
     "operatorsAliases": 0
+    
   },
   "production": {
     "username": process.env.DB_USER,
