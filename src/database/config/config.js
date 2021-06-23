ecdcac3c749f16ddef0056f5f9e45f8671a7ccb1
sbarciela <barcielasebastian@gmail.com>
2021-06-23 16:51:35 -0300

    agregando configuraciones para deploy

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,23 +1,29 @@
+require('dotenv').config();
+
 module.exports = {
   "development": {
-    "username": "root",
-    "password": "root",
-    "database": "kicks_db",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "port":process.env.DB_PORT,
+    "dialect": "mysql",
+    "operatorsAliases": 0
   },
   "test": {
     "username": "root",
     "password": null,
     "database": "database_test",
     "host": "127.0.0.1",
-    "dialect": "mysql"
+    "dialect": "mysql",
+    "operatorsAliases": 0
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "dialect": "mysql",
+    "operatorsAliases": 0
   }
 }
